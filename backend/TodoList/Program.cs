ce4300a5b2ccee4ee4c4a65e8b1e8f20f4c1198a
gabriel morales uribe <gabrielmoralesuribe530@gmail.com>
2026-02-18 22:09:39 -0500

    Fixed CORS, Front and Backend functional

--- backend/TodoList/Program.cs
+++ backend/TodoList/Program.cs
@@ -3,53 +3,35 @@ using TodoListDataLayer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Add in the database
 var connectionString = builder.Configuration.GetConnectionString("TodoListDbConnection");
 builder.Services.AddDbContext<TodoListDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Add CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins(
-                "http://localhost:5173",
-                "https://blue-rock-0a91f240f.1.azurestaticapps.net"
-            )
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
 });
+
 var app = builder.Build();
 
-// Apply pending migrations on startup
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<TodoListDbContext>();
-    dbContext.Database.Migrate();
-}
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseCors("AllowFrontend");
-
 app.UseHttpsRedirection();
 
+// ADD THIS - must be before UseAuthorization and MapControllers
+app.UseCors("AllowFrontend");
+
 app.UseAuthorization();
-
 app.MapControllers();
-
-
-
 app.Run();
